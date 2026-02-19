367d17ce5f525da260efcc69e217ccaab177cd1a
Mauro <mauro_9_2@hotmail.com>
2026-02-19 17:02:39 -0500

    ♻️ change fallback to false

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -30,7 +30,7 @@ export async function getStaticPaths() {
   client.close();
 
   return {
-    fallback: true,
+    fallback: false,
     paths: meetups.map((meetup) => ({
       params: { meetupId: meetup._id.toString() },
     })),
